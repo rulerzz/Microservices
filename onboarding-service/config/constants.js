3ba834f1635e42c12dca474c950f2fc0d81a23b4
rulerzz <may.ghz.conan@gmail.com>
2023-08-30 22:34:12 +0530

    added signup page:

--- onboarding-service/config/constants.js
+++ onboarding-service/config/constants.js
@@ -10,5 +10,5 @@ module.exports = {
     HEALTHURL: `/${process.env.SERVICE_VERSION}/${process.env.SERVICE_PREFIX}/health`,
     HOST_IP: process.env.HOST_IP ? process.env.HOST_IP : "localhost",
     CONSUL_PORT: process.env.CONSUL_PORT ? process.env.CONSUL_PORT : 8500,
-    SERVICE_PORT: process.env.PORT ? process.env.PORT: 5001
+    SERVICE_PORT: process.env.SERVICE_PORT ? process.env.SERVICE_PORT: 2001
 };
